bdb63b975141a3389192f27e23b93d9908943cf6
ahyeon <uiyn@naver.com>
2021-08-28 16:13:04 +0900

    BOJ_3052 나머지

---
+++ src/com/algo/boj/Main_3052.java
@@ -0,0 +1,23 @@
+package com.algo.boj;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * 
+ * BOJ_3052 나머지
+ *
+ */
+public class Main_3052 {
+
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			set.add(sc.nextInt() % 42);
+		}
+		System.out.println(set.size());
+
+	}
+
+}
